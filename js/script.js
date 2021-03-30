5cfdc26a0e4d40bc2f6d4c7bed3496617c9a4d74
Daniele Alessi <daniele.alessi@hotmail.it>
2021-03-30 17:00:14 +0200

    inserito risultato con le condizioni

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
-// tramite il pulsante calcola devo fare in modo di calcolare il prezzo finale del prodotto tenendo conto dei prodotti aggiunti selezionati e di eventuali coupon 
-
-// genero variabile per il prezzo finale che di default è 50$ 
-var finalPrice = 50;
-
+// tramite il pulsante 'calcola' devo fare in modo di calcolare il prezzo finale del prodotto tenendo conto dei prodotti aggiunti selezionati e di eventuali coupon 
 // attivo il pulsante calculate 
 
 // assegno variabile button a pulsante html 
 var button = document.getElementById('calculate');
-console.log(button);
 
 // metto in ascolto pulsante e genero codice al suo interno 
 button.addEventListener('click', function(){
     
-    // genero variabile per lavorare con la funzione dei checkbox tutti insieme 
+    var finalPrice = 50;
+
+    // genero variabile, selezionando le classi degli elementi html, per lavorare con la funzione dei checkbox tutti insieme (risultato --> array)
     var addChecked = document.getElementsByClassName('add');
-    console.log(addChecked);
 
     // inserisco un ciclo per leggere ogni elemento dell'array di elementi generata 
     for ( var i = 0; i < addChecked.length; i++) {
-        var thisAdd = addChecked[i];
-        console.log(thisAdd);
+        var thisAddChecked = addChecked[i];
+        // console.log(thisAddChecked.checked);
+        
+        var thisAddPrice = parseInt(thisAddChecked.value);
+        // console.log(thisAddPrice);
+        
+        if (thisAddChecked.checked == true) {
+            finalPrice = finalPrice + thisAddPrice;
+        }
+    
     }
+    console.log(finalPrice);
+
+    // assegno il risultato all'elemento html 
+    document.getElementById("final-price").innerHTML = finalPrice + " $";
 })
